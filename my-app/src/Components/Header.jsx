575c259319efbf37d3b111e53badfd88a015aade
Diesfingers228 <dimaarel2@gmail.com>
2023-08-17 14:40:07 +0300

    Life_Gamer

--- my-app/src/Components/Header.jsx
+++ my-app/src/Components/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../App.css';
 import '../AppMobile.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { headerStyle, windowClick} from '../Store/Slices/FirstSlice';
 import Right from '../Icons_and_photos/Right.svg';
+import Rei from '../Icons_and_photos/Rei.jpg'
 
 function Header() {
-let navigate = useNavigate();
 
 // Store
 const selector = useSelector(state => state.FirstReducer.bar);
@@ -17,11 +17,13 @@ const dispatch = useDispatch();
   return (
     <>
     <header style={selector}  className='header'>
-       <label onClick={() => {navigate(0)}} style={{fontSize: '150%'}}>&raquo;<Link to='/'>Главная</Link></label>
+       <label style={{fontSize: '150%'}}>&raquo;<Link to='/'>Главная</Link></label>
        <label style={{fontSize: '150%'}}>&raquo;<Link to='/info'>Информация</Link></label>
     </header>
 
     <img style={selectorImg} src={Right} className='menu-img' onClick={() => {dispatch(headerStyle());}}/>
+
+    <img className='rei-spin' src={Rei}/>
    </>
   )
 }
